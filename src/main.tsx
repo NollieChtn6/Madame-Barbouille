8d14ac47635a6d6f66d4998560ab0965befbc1f5
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-07-04 07:08:21 +0200

    configure project

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+// https://www.codingdeft.com/posts/react-18-typescript-error/
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
